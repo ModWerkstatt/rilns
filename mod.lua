d0babc0c1bc89c47a582710b1f97e38b8b48a07f
JEmanuel1806 <janplischke97@gmail.com>
2025-05-01 18:34:05 +0200

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,35 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = 'jay_',
+				role = 'CREATOR',
+				text = 'Modell',
+				tfnetId = 28954,
+			},
+					    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		tags = { "europe", "waggon", "deutschland", "germany", "gueterwagen", "db", "limms", "astrans" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+
+    end
+
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,34 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Special wagon for glass transport (Limms)",
+		["mod_desc"] = "Increasing environmental pollution and the high demand for mobility in Europe have led to a search for ways to shift freight traffic from roads to rail. Until now, large-format flat glass in Europe has mainly been transported by special trucks. However, AStrans and Pilkington Inc. have developed an innovative solution using their own special wagons with special fixtures and a non-slip floor to transport flat glass by rail. These wagons are environmentally friendly and enable the efficient transport of flat glass between Scandinavia, England and mainland Europe. For this innovation, AStrans received the European Transport Award in 1998.",
+		["gedreht"] = "(rotated) ",
+		["sound_check"] = "Activate custom sound?",
+		["height"] = "Height",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+		["rils_desc"] = "The first DB wagons with a tarpaulin cover (Rils 652) were created in 1988/89 at the Paderborn repair works by converting Res 687 flat wagons. During the conversion, stanchions, side walls, and floors were removed and replaced with new wooden floors. The fixed end walls and the characteristic blue textile tarpaulin cover were later installed by Talbot. In 1990/91, another 400 conversions followed, carried out by the Eberswalde repair works and private companies. These later wagons are recognizable by their tarpaulin covers with a central locking system, allowing easy opening and closing from one side. Since 1997, damaged tarpaulins have been replaced with ones in traffic red.",
+		["rilns_desc"] = "The Rilns is a modern four-axle freight car with fixed end walls and a sliding tarpaulin cover, built from 2001 onwards by Tatravagónka Poprad for operators like AAE (later VTG) and other private companies such as CFL Cargo. It is designed for weather-protected transport of large, bulky goods like machinery, steel products, or paper rolls. The wagon is rated for a maximum axle load of 25 tons and a top speed of 120 km/h. Key features include the robust, weather-resistant tarpaulin system and modern Y25 bogies, ensuring high stability and safety for international operations.",
+
+	},
+	de = {
+
+		["mod_name"] = "Schiebeplanenwagen Ril(n)s",
+		["mod_desc"] = "Die steigende Umweltbelastung und der hohe Mobilitätsbedarf in Europa haben dazu geführt, dass nach Möglichkeiten gesucht wurde, den Frachtverkehr von Straßen auf die Schiene zu verlagern. Bisher wurde großformatiges Flachglas in Europa hauptsächlich mit speziellen Lastwagen transportiert. AStrans und Pilkington Inc. haben jedoch eine innovative Lösung entwickelt, bei der eigene Spezialwaggons mit speziellen Vorrichtungen und einem rutschfesten Boden für den Transport von Flachglas auf der Schiene verwendet werden. Diese Waggons sind umweltfreundlich und ermöglichen den effizienten Transport von Flachglas zwischen Skandinavien, England und dem europäischen Festland. Für diese Innovation erhielt AStrans 1998 den Europäischen Transportpreis.",
+		["gedreht"] = "(gedreht) ",
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["height"] = "Höhe",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+		["rils_desc"] = "Die ersten Wagen der DB mit Planenhaube (Rils 652) der DB entstanden 1988/89 im Ausbesserungswerk Paderborn durch den Umbau von Res 687-Flachwagen. Dabei wurden Rungen, Bordwände und Böden entfernt und durch neue Bodenbretter ersetzt. Stirnwände und die charakteristische blaue Textilplanenhaube wurden anschließend bei Talbot angebaut. 1990/91 folgten weitere 400 Umbauten durch das Raw Eberswalde und Privatfirmen, erkennbar an der Planenhaube mit Zentralverriegelung, die sich bequem von einer Seite öffnen und schließen lässt. Seit 1997 werden beschädigte Planen durch verkehrsrote ersetzt.",
+		["rilns_desc"] = "Der Rilns ist ein moderner 4-achsiger Güterwagen mit festen Stirnwänden und verschiebbarer Planenhaube, gebaut ab 2001 von Tatravagónka Poprad für den Einsteller AAE (später VTG) sowie weitere private Betreiber wie CFL Cargo. Er eignet sich für den witterungsgeschützten Transport von großen, sperrigen Gütern wie Maschinen, Stahl- oder Papierrollen. Der Wagen ist für eine maximale Achslast von 25 Tonnen und eine Höchstgeschwindigkeit von 120 km/h ausgelegt. Charakteristisch sind die robuste, wetterfeste Planenkonstruktion und die modernen Y25-Drehgestelle, die hohe Stabilität und Sicherheit im internationalen Verkehr gewährleisten.",
+
+	},
+}
+end
